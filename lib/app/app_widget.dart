b23db57a188fc4119c409a957bd9a6b748d3137b
Henrique <ap_henrique@hotmail.com>
2022-03-01 16:16:57 -0300

    Fix modular router

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:masterclass_presentation/app/core/utils/app_themes.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
-    ).modular();
+      theme: AppThemes.darkTheme,
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
   }
 }
