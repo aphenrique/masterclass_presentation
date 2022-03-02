b7df4c1b89273baa74baf2141ec5f18a0dc43022
Henrique <ap_henrique@hotmail.com>
2022-03-01 21:37:37 -0300

    Add appbar theme

--- lib/app/core/utils/themes/app_dark_theme.dart
+++ lib/app/core/utils/themes/app_dark_theme.dart
@@ -6,13 +6,23 @@ class AppDarkTheme {
   static final darkTheme = ThemeData(
     primarySwatch: AppColors.primaryColor,
     scaffoldBackgroundColor: AppColors.darkBackground,
+    fontFamily: 'montserrat',
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.darkBackground,
+      elevation: 8,
+    ),
     textTheme: TextTheme(
       headline1: AppBaseTheme.textH1.copyWith(color: AppColors.darkHighlight),
       headline2: AppBaseTheme.textH2.copyWith(color: AppColors.darkHighlight),
+      headline3: AppBaseTheme.textH3.copyWith(color: AppColors.darkHighlight),
+      headline4: AppBaseTheme.textH4.copyWith(color: AppColors.darkHighlight),
       bodyText1:
           AppBaseTheme.bodyText1.copyWith(color: AppColors.darkHighlight),
       bodyText2:
           AppBaseTheme.description.copyWith(color: AppColors.darkHighlight),
+    ),
+    listTileTheme: const ListTileThemeData(
+      textColor: AppColors.darkHighlight,
     ),
   );
 }
