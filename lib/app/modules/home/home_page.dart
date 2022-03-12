23d4018a2c7657c64e313fb53ad6d78ed8eb0f7e
Henrique <ap_henrique@hotmail.com>
2022-03-12 14:31:15 -0300

    Implements thems on Material app

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:masterclass_presentation/app/core/components/theme_changer_component.dart';
+import 'package:masterclass_presentation/app/core/utils/images/app_images.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,9 +13,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      appBar: AppBar(
+        title: ListTile(
+          leading: Image.asset(AppImages.logo),
+          title: Text(
+            'Atividades',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          subtitle: Text(
+            'Flutterando Masterclass',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        actions: const [ThemeChangerComponent()],
+      ),
+      body: Center(
+        child: Container(),
       ),
     );
   }
